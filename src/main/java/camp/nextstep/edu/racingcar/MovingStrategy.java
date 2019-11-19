9c49a7da6ba1b30bdf631d337ac073816b294fc2
yunjey0 <sehajyang@gmail.com>
2019-11-19 22:32:54 +0900

    test : Car 통합 테스트코드 작성

---
+++ src/main/java/camp/nextstep/edu/racingcar/MovingStrategy.java
@@ -0,0 +1,4 @@
+package camp.nextstep.edu.racingcar;
+
+public interface MovingStrategy {
+}
--- src/main/java/camp/nextstep/edu/racingcar/RandomMovingStrategy.java
+++ src/main/java/camp/nextstep/edu/racingcar/RandomMovingStrategy.java
@@ -2,7 +2,7 @@ package camp.nextstep.edu.racingcar;
 
 import java.util.Random;
 
-public class RandomMovingStrategy {
+public class RandomMovingStrategy implements MovingStrategy {
     boolean movable() {
         return new Random().nextInt(9) >= 4;
     }
