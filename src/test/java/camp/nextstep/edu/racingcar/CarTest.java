f085751a19539fe7454b346af20300c186adb0a5
venti <54702844+jwee0330@users.noreply.github.com>
2019-11-19 22:45:45 +0900

    feat 자동차 step0 작성 (#10)
    * 자동차 이름이 5자 이하인 테스트 작성
     * 자동차 이름이 5자 이상인 테스트 작성
     * 자동차 움직이는 테스트 작성
     * 자동차 움직이지 않는 테스트 작성
    
    Signed-off-by: 이정원 <jwee0330@gmail.com>

---
+++ src/test/java/camp/nextstep/edu/racingcar/CarTest.java
@@ -0,0 +1,51 @@
+package camp.nextstep.edu.racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class CarTest {
+
+    @DisplayName("자동차 생성")
+    @Test
+    void create() {
+        new Car("정원", 1);
+    }
+
+
+    @DisplayName("자동차 이름 5자 넘지 않는 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"aaa", "bbbb", "cccc"})
+    void testValidCarName(String carName) {
+        new Car(carName);
+    }
+
+
+    @DisplayName("자동차 이름 5자 넘는 테스트")
+    @Test
+    void testInvalidCarName() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car("abcdef"));
+    }
+
+    @DisplayName("자동차가 움직이지 않는 테스트")
+    @Test
+    void notMove() {
+        Car car = new Car("정원", 0);
+        car.move(()-> false);
+        assertThat(car.isInPosition(0)).isTrue();
+    }
+
+    @DisplayName("자동차가 움직이는 테스트")
+    @Test
+    void move() {
+        Car car = new Car("정원", 0);
+        car.move(()-> true);
+        assertThat(car.isInPosition(1)).isTrue();
+    }
+
+}
