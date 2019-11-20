23c142d5fcc759f4ba5d9b557cf7967eace1dd84
Juyoung <juy5790@outlook.com>
2019-11-21 00:01:10 +0900

    feat 구분자(, or :)로 하여 합 반환 구현

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -8,12 +8,15 @@ import java.util.Objects;
 public class StringCalculator implements Calculator {
 
     static final int ZERO = 0;
+    static final String SEPARATOR = ",|:";
 
     @Override
     public int add(String expression) {
         if(Objects.isNull(Strings.trimToNull(expression))){
             return ZERO;
         }
-        return Integer.parseInt(expression);
+        return Arrays.stream(expression.split(SEPARATOR))
+              .mapToInt(Integer::valueOf)
+              .sum();
     }
 }
--- src/test/java/camp/nextstep/edu/calculator/StringCalculatorTest.java
+++ src/test/java/camp/nextstep/edu/calculator/StringCalculatorTest.java
@@ -2,10 +2,13 @@ package camp.nextstep.edu.calculator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static camp.nextstep.edu.calculator.StringCalculator.SEPARATOR;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StringCalculatorTest {
@@ -30,5 +33,16 @@ class StringCalculatorTest {
     @ValueSource(strings = {"1", "11", "123"})
     void add_whenInputOne(String inputOfNumber) {
         assertThat(calculator.add(inputOfNumber)).isEqualTo(Integer.parseInt(inputOfNumber));
+    }
+
+    @DisplayName(SEPARATOR + "를 구분자로 하여 숫자의 합을 반환")
+    @ParameterizedTest
+    @CsvSource({
+            "'1,2',           3",
+            "'1,10,100',    111",
+            "'1,10:100',    111",
+    })
+    void add_whenSeparatorComma(String expression, int expectedResult) {
+        assertThat(calculator.add(expression)).isEqualTo(expectedResult);
     }
 }
