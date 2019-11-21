7b873720b96e7e7e784c3db0a9acf97e8d5c9ab6
daeheejeong <dev.james.jeong@gmail.com>
2019-11-21 13:35:33 +0900

    feat(calculator): 구분자를 컴마 이외에 콜론 이용 가능

--- src/test/java/camp/nextstep/edu/calculator/CalculatorTest.java
+++ src/test/java/camp/nextstep/edu/calculator/CalculatorTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Value;
 
 class CalculatorTest {
 
@@ -40,5 +43,13 @@ class CalculatorTest {
     void inputIsTwoNumbers() {
         Assertions.assertThat(calculator.add("1,2"))
                 .isEqualTo(3);
+    }
+
+    @DisplayName("구분자를 컴마 이외에 콜론을 사용할 수 있다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,5", "2:4", "1,2:3"})
+    void commaAndColonAsDelimiter(String input) {
+        Assertions.assertThat(calculator.add(input))
+                .isEqualTo(6);
     }
 }
