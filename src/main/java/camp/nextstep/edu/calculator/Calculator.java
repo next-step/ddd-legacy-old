3a6eda375b7d5a5971a73fb39ed300f1728eea31
daeheejeong <dev.james.jeong@gmail.com>
2019-11-21 15:50:08 +0900

    feat(calculator): 커스텀 구분자 지정 가능

--- src/main/java/camp/nextstep/edu/calculator/Calculator.java
+++ src/main/java/camp/nextstep/edu/calculator/Calculator.java
@@ -1,17 +1,35 @@
 package camp.nextstep.edu.calculator;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
+
+    private static final String DELIMITER_REGEX_CUSTOM = "//(.)\\n(.*)";
 
     public int add(String input) {
         if (isEmptyString(input)) {
             return 0;
         }
 
-        String[] numbers = input.split("\\,|\\:");
+        String[] numbers = split(input);
 
         return getSum(numbers);
+    }
+
+    private String[] split(String input) {
+        String delimiter = "\\,|\\:";
+
+        Matcher matcher = Pattern.compile(DELIMITER_REGEX_CUSTOM)
+                .matcher(input);
+
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+            input = matcher.group(2);
+        }
+
+        return input.split(delimiter);
     }
 
     private int getSum(String[] numbers) {
@@ -25,4 +43,6 @@ public class Calculator {
     private boolean isEmptyString(String str) {
         return Objects.isNull(str) || str.isEmpty();
     }
+
+
 }
