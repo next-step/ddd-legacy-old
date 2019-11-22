5bb0b0b9b72b53541f6b5283af94c64b33ddc27d
kingbbode <kingbbode@woowahan.com>
2019-11-23 01:45:23 +0900

    feat:커스텀 구분자, 피연산자 empty 보완.

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -7,26 +7,22 @@ import org.springframework.util.StringUtils;
 
 public class StringCalculator {
 
-    private static final String DELIMITER_REGEX = "[,:]";
-    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.*)\n(.*)");
-
     public int calc(String source) {
-        if(StringUtils.isEmpty(source)) {
+        StringFormula formula = StringFormula.parse(source);
+
+        if(formula.isEmpty()) {
             return 0;
         }
-        String[] operands = extractOperands(source);
 
+        String[] operands = formula.extractOperands();
+
+        return sum(operands);
+    }
+
+    private int sum(String[] operands) {
         return Arrays.stream(operands)
             .mapToInt(StringCalculator::parsePositiveInt)
             .sum();
-    }
-
-    private String[] extractOperands(String source) {
-        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(source);
-        if(matcher.find()) {
-            return matcher.group(2).split(matcher.group(1));
-        }
-        return source.split(DELIMITER_REGEX);
     }
 
     private static int parsePositiveInt(String s) throws NumberFormatException {
@@ -35,5 +31,38 @@ public class StringCalculator {
             throw new NumberFormatException("It is not positive numbers. string: \"" + s + "\"");
         }
         return i;
+    }
+
+    static class StringFormula {
+        private static final String DELIMITER_REGEX = "[,:]";
+        private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.*)\n(.*)");
+        static final StringFormula EMPTY = new StringFormula(DELIMITER_REGEX, "");
+
+        private String delimiter;
+        private String formula;
+
+        private StringFormula(String delimiter, String formula) {
+            this.delimiter = delimiter;
+            this.formula = formula;
+        }
+
+        static StringFormula parse(String source) {
+            if(StringUtils.isEmpty(source)) {
+                return EMPTY;
+            }
+            Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(source);
+            if(matcher.find()) {
+                return new StringFormula(matcher.group(1), matcher.group(2));
+            }
+            return new StringFormula(DELIMITER_REGEX, source);
+        }
+
+        boolean isEmpty() {
+            return StringUtils.isEmpty(formula);
+        }
+
+        String[] extractOperands() {
+            return formula.split(delimiter);
+        }
     }
 }
