1e261c06822dce2b725e8e68e40ed6530b5014b3
kingbbode <kingbbode@woowahan.com>
2019-11-23 00:53:42 +0900

    feat:기본 구분자(',',':') 를 통한 합산

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -1,8 +1,20 @@
 package camp.nextstep.edu.calculator;
 
+import java.util.Arrays;
+import org.springframework.util.StringUtils;
+
 public class StringCalculator {
 
+    private static final String DELIMITER_REGEX = "[,:]";
+
     public int calc(String source) {
-        return 0;
+        if(StringUtils.isEmpty(source)) {
+            return 0;
+        }
+        String[] split = source.split(DELIMITER_REGEX);
+
+        return Arrays.stream(split)
+            .mapToInt(Integer::parseInt)
+            .sum();
     }
 }
