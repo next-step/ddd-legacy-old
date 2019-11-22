85dc4bd400c8fc9a89cd51aac5ef0bce3757b1c0
kingbbode <kingbbode@woowahan.com>
2019-11-23 01:30:01 +0900

    feat:커스텀 구분자를 통한 합산

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -1,21 +1,32 @@
 package camp.nextstep.edu.calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.springframework.util.StringUtils;
 
 public class StringCalculator {
 
     private static final String DELIMITER_REGEX = "[,:]";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.*)\n(.*)");
 
     public int calc(String source) {
         if(StringUtils.isEmpty(source)) {
             return 0;
         }
-        String[] split = source.split(DELIMITER_REGEX);
+        String[] operands = extractOperands(source);
 
-        return Arrays.stream(split)
+        return Arrays.stream(operands)
             .mapToInt(StringCalculator::parsePositiveInt)
             .sum();
+    }
+
+    private String[] extractOperands(String source) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(source);
+        if(matcher.find()) {
+            return matcher.group(2).split(matcher.group(1));
+        }
+        return source.split(DELIMITER_REGEX);
     }
 
     private static int parsePositiveInt(String s) throws NumberFormatException {
