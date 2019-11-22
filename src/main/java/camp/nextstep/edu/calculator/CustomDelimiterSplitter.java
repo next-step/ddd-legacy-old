650d81f07008c83d540a94e5b56f0c1b347a1341
mouthgaebal <mouthgaebal@gmail.com>
2019-11-23 01:33:47 +0900

    feat : 커스텀 구분자 지정 가능한 Splitter 구현

---
+++ src/main/java/camp/nextstep/edu/calculator/CustomDelimiterSplitter.java
@@ -0,0 +1,41 @@
+package camp.nextstep.edu.calculator;
+
+import camp.nextstep.edu.util.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CustomDelimiterSplitter implements Splitter {
+
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    @Override
+    public boolean supports(String stringValue) {
+        if (StringUtils.isBlank(stringValue)) {
+            return false;
+        }
+        return CUSTOM_DELIMITER_PATTERN.matcher(stringValue).matches();
+    }
+
+    @Override
+    public String[] split(String stringValue) {
+        final Matcher matcher = createSplitMatcher(stringValue);
+
+        final String delimiter = matcher.group(1);
+        final String values = matcher.group(2);
+        return values.split(Pattern.quote(delimiter));
+    }
+
+    private Matcher createSplitMatcher(String stringValue) {
+        final String errorMessage = "지원하지 않는 형식의 값입니다. stringValue : [" + stringValue + "]";
+        if (StringUtils.isBlank(stringValue)) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        final Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(stringValue);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+        return matcher;
+    }
+}
