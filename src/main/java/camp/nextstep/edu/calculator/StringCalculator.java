b34111883121d5abf473519b0749bf89d4848dfe
Summer <jihan805@coupang.com>
2019-11-24 00:39:01 +0900

    feat : 요구사항 3 구현

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -1,11 +1,17 @@
 package camp.nextstep.edu.calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public int add(String input) {
         if (input == null || input.isEmpty())
             return 0;
 
-        return Integer.parseInt(input);
+        String[] numbers = input.split(",");
+
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
