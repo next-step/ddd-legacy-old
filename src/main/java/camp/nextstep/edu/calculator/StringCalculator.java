398b9729ac46e56e598ea7c879777ebd643d5b62
doooyeon <ehdus5958@naver.com>
2019-11-24 01:25:10 +0900

    feat: 지정한 구분자로 문자열 나누기

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -1,8 +1,12 @@
 package camp.nextstep.edu.calculator;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
 
     public int add(final String text) {
         if (isEmptyOrNull(text)) return 0;
@@ -11,5 +15,15 @@ public class StringCalculator {
 
     private boolean isEmptyOrNull(final String text) {
         return Objects.isNull(text) || text.isEmpty();
+    }
+
+    private String[] split(final String text) {
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+
+        return text.split(DEFAULT_DELIMITER_REGEX);
     }
 }
