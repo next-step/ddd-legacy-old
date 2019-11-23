2e044c087e026417cf64261eb03e7f1d04e9ad57
Summer <jihan805@coupang.com>
2019-11-24 01:04:48 +0900

    feat : 요구사항 5 구현

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -1,13 +1,29 @@
 package camp.nextstep.edu.calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     private static final String DELIMITER = "[,:]";
+    private static final String REGEX = "//(.)\n(.*)";
 
     public int add(String input) {
         if (input == null || input.isEmpty())
             return 0;
+
+
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            String[] tokens = matcher.group(2).split(customDelimiter);
+
+            return Arrays.stream(tokens)
+                    .mapToInt(Integer::parseInt)
+                    .sum();
+        }
 
         String[] numbers = input.split(DELIMITER);
 
