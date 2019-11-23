3bedcdb52c351f3d4f2dcaa73e4d6415eeb1638f
Summer <jihan805@coupang.com>
2019-11-24 00:50:48 +0900

    feat : 요구사항 4 구현

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -3,12 +3,13 @@ package camp.nextstep.edu.calculator;
 import java.util.Arrays;
 
 public class StringCalculator {
+    private static final String DELIMITER = "[,:]";
 
     public int add(String input) {
         if (input == null || input.isEmpty())
             return 0;
 
-        String[] numbers = input.split(",");
+        String[] numbers = input.split(DELIMITER);
 
         return Arrays.stream(numbers)
                 .mapToInt(Integer::parseInt)
