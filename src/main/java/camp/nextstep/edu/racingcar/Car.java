efd4cb92017544c5163b7b65711e5914d5181a6a
sunghyun.keum <sunghyun.keum@woowahan.com>
2019-11-25 02:18:22 +0900

    test(car): total test

--- src/main/java/camp/nextstep/edu/racingcar/Car.java
+++ src/main/java/camp/nextstep/edu/racingcar/Car.java
@@ -14,7 +14,7 @@ public class Car {
         this.position = position;
     }
 
-    void move(final RandomMovingStrategy movingStrategy) {
+    void move(MovingStrategy movingStrategy) {
         if (movingStrategy.movable()) {
             position++;
         }
