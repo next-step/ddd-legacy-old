59e2ec1cf0f4ac94e7853b277176a9d9e2fc22cc
captaintailer <captaintailer@gmail.com>
2019-11-24 20:06:40 +0900

    test(StringCalculator): 문자열 계산기 덧셈 테스트

---
+++ src/test/java/camp/nextstep/edu/calculator/StringCalculatorTest.java
@@ -0,0 +1,71 @@
+package camp.nextstep.edu.calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class StringCalculatorTest {
+
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyOrNull(final String text) {
+        assertThat(calculator.add(text)).isZero();
+    }
+
+    @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "1", "65535", "2147483647"})
+    void oneNumber(final String text) {
+        assertThat(calculator.add(text)).isEqualTo(Integer.parseInt(text));
+    }
+
+    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2"})
+    void twoNumbers(final String text) {
+        assertThat(calculator.add(text)).isSameAs(3);
+    }
+
+    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2:3"})
+    void colons(final String text) {
+        assertThat(calculator.add(text)).isSameAs(6);
+    }
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3", "//blahblah\n1blahblah2blahblah3"})
+    void customDelimiter(final String text) {
+        assertThat(calculator.add(text)).isSameAs(6);
+    }
+
+    @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @Test
+    void negative() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> calculator.add("-1"));
+    }
+
+    @DisplayName(value = "문자열 계산기에 유효하지 않은 값을 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"////;\n1;2;3", "1|2|3", "1/2/3"})
+    void invalidValue(final String text) {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> calculator.add(text));
+    }
+}
