5df6f0e1a967b051de0ac30746b7540920d58daf
조정훈 <jeonghoon.cho@woowahan.com>
2019-11-24 16:43:14 +0900

    [요구사항 2] 앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다.

--- src/main/java/camp/nextstep/edu/calculator/Calculator.java
+++ src/main/java/camp/nextstep/edu/calculator/Calculator.java
@@ -1,14 +1,35 @@
 package camp.nextstep.edu.calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
     private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+    private final Pattern pattern = Pattern.compile(CUSTOM_DELIMITER_PATTERN);
 
     public int add(String text) {
-        return Arrays.stream(text.split(DEFAULT_DELIMITER))
+
+        Matcher m = pattern.matcher(text);
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return sum(parse(m.group(2), customDelimiter));
+        }
+
+        return sum(parse(text, DEFAULT_DELIMITER));
+    }
+
+    private int sum(String[] values) {
+        return Arrays.stream(values)
                 .mapToInt(Integer::valueOf)
                 .sum();
     }
+
+    private String[] parse(String text, String delimiter) {
+        return text.split(delimiter);
+    }
+
 }
--- src/test/java/camp/nextstep/edu/calculator/CalculatorTest.java
+++ src/test/java/camp/nextstep/edu/calculator/CalculatorTest.java
@@ -3,7 +3,11 @@ package camp.nextstep.edu.calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,6 +25,21 @@ class CalculatorTest {
     @CsvSource(value = {"1,2:3=6", "5,3,2=10", "2:3:4=9", "0:0,0=0"}, delimiter = '=')
     void 기본_문자열_계산기(String text, Integer expected) {
         assertThat(calculator.add(text)).isEqualTo(expected);
+    }
+
+    @DisplayName("커스텀 문자열 계산기")
+    @ParameterizedTest
+    @MethodSource
+    void 커스텀_문자열_계산기(String text, Integer expected) {
+        assertThat(calculator.add(text)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> 커스텀_문자열_계산기() {
+        return Stream.of(
+                Arguments.of("//;\n1;2;3", 6),
+                Arguments.of("//&\n4&3&2", 9),
+                Arguments.of("//#\n2#4#8", 14)
+        );
     }
 
 }
