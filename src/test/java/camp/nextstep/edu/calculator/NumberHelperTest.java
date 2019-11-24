a6eb5da061f196f41a670cc14d149a9dc53bda47
teachd000000 <teachd000000@gmail.com>
2019-11-25 00:05:37 +0900

    refactor(NumberCalculator): modify @DisplayName

--- src/test/java/camp/nextstep/edu/calculator/NumberHelperTest.java
+++ src/test/java/camp/nextstep/edu/calculator/NumberHelperTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class NumberHelperTest {
 
-    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 true 를 반환해야 한다.")
+    @DisplayName(value = "음수 값을 입력할 경우 true 를 반환해야 한다.")
     @ParameterizedTest
     @ValueSource(ints = {-1})
     void isNegative(final int number) {
