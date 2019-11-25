92d2dcfe2451f216164bde347f01db5f9025b6b3
Summer <jihanbaek805@gmail.com>
2019-11-25 21:58:19 +0900

    Step1 리뷰요청드립니다. (#46)
    * feat : 요구사항 1, 2 구현
    
    * feat : 요구사항 3 구현
    
    * feat : 요구사항 4 구현
    
    * feat : 요구사항 5 구현
    
    * refactor : 문자열 계산기 리팩토링
    
    * feat : 요구사항 6 구현
    
    * style : 괄호 추가
    
    * refactor : Objects, Pattern 피드백 반영
    
    * refactor : Number class 분리
    
    * style : import  정리
    
    * refactor : sum() 리팩토링
    
    * refactor : sum() 리팩토링2
    
    * refactor : 미사용 상수 제거
    
    * style : space convention

---
+++ src/main/java/camp/nextstep/edu/calculator/Number.java
@@ -0,0 +1,31 @@
+package camp.nextstep.edu.calculator;
+
+public class Number {
+
+    private int number;
+
+    private Number(int number) {
+        this.number = number;
+    }
+
+    public static Number of(String input) {
+        int number = Integer.parseInt(input);
+        validatePositiveNumber(number);
+
+        return new Number(number);
+    }
+
+    private static void validatePositiveNumber(int number) {
+        if (number < 0) {
+            throw new RuntimeException("문자열 계산기에 음수 사용은 불가합니다.");
+        }
+    }
+
+    public int toInt() {
+        return number;
+    }
+
+    public Number add(Number input) {
+        return new Number(this.number + input.number);
+    }
+}
---
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -0,0 +1,54 @@
+package camp.nextstep.edu.calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.toList;
+
+public class StringCalculator {
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String REGEX = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER = Pattern.compile(REGEX);
+
+    private static final int DEFAULT_VALUE = 0;
+
+    public int add(String input) {
+        if (isEmpty(input)) {
+            return DEFAULT_VALUE;
+        }
+
+        String[] numbers = parse(input);
+        List<Number> convertedNumbers = convert(numbers);
+        return sum(convertedNumbers);
+    }
+
+    private boolean isEmpty(String input) {
+        return Objects.isNull(input) || input.isEmpty();
+    }
+
+    private String[] parse(String input) {
+        Matcher matcher = CUSTOM_DELIMITER.matcher(input);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+
+        return input.split(DEFAULT_DELIMITER);
+    }
+
+    private List<Number> convert(String[] numbers) {
+        return Arrays.stream(numbers)
+                .map(Number::of)
+                .collect(toList());
+    }
+
+    private int sum(List<Number> numbers) {
+        return numbers.stream()
+                .mapToInt(Number::toInt)
+                .sum();
+    }
+}
