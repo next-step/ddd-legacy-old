c23f1093f65c3edee7ddc099a84ef1366b6832ab
sehajyang <sehajyang@gmail.com>
2019-11-25 15:11:23 +0900

    feat : sum 함수 작성, splitNumberByToken 함수 수정

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
     private static final Pattern CUSTOM_TOKEN = Pattern.compile("//(.)\n(.*)");
+    private static final String DEFAULT_TOKEN = "[,:]";
 
     public int add(String text) {
         if (!isEmpty(text)) {
@@ -14,20 +15,27 @@ public class StringCalculator {
     }
 
     private int sum(String text) {
-        ;
+        String[] numbers = splitNumberByToken(text);
+        int result = 0;
+        for (String numberItem : numbers) {
+            int number = Integer.parseInt(numberItem);
+            checkNegativeNumber(number);
+            result += number;
+        }
+        return result;
     }
 
     private String[] splitNumberByToken(String text) {
-        Matcher m = CUSTOM_TOKEN.matcher(text);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] tokens= m.group(2).split(customDelimiter);
+        Matcher matcher = CUSTOM_TOKEN.matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
         }
-        return text.split("[,:]");
+        return text.split(DEFAULT_TOKEN);
     }
 
-    private void checkNegativeNumber(int number){
-        if(number < 0){
+    private void checkNegativeNumber(int number) {
+        if (number < 0) {
             throw new RuntimeException();
         }
     }
