0f6c10f16e4730b79f32ab54ae4b2eb21f8920fc
Ryan.an <giveawesome@gmail.com>
2019-11-25 15:21:48 +0900

    feat(StringCalculator): "add" method to use custom delimeter

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -1,11 +1,14 @@
 package camp.nextstep.edu.calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.springframework.util.StringUtils;
 
 public class StringCalculator {
 
 	private static final String DELIMITER = ",|:";
+	private static final String PATTERN_DELIMITER = "//(.)\n(.*)";
 
 	/**
 	 * Add string of integer numbers which is included with comma(,) or colon(:)
@@ -18,10 +21,37 @@ public class StringCalculator {
 			return 0;
 		}
 
-		return Arrays.stream(numbersText.split(DELIMITER))
+		Matcher matcher = Pattern.compile(PATTERN_DELIMITER).matcher(numbersText);
+
+		if (matcher.find()) {
+			String patternDelimeter = matcher.group(1);
+			return sum(split(matcher.group(2), patternDelimeter));
+		}
+
+		return sum(split(numbersText, DELIMITER));
+	}
+
+	/**
+	 * Sum of integer numbers
+	 *
+	 * @param numbers
+	 * @return sum of integer numbers
+	 */
+	private int sum(String[] numbers) {
+		return Arrays.stream(numbers)
 			.mapToInt(Integer::valueOf)
 			.sum();
 	}
 
+	/**
+	 * Split string of integer numbers which is included with delimiter
+	 *
+	 * @param numbersText
+	 * @param delimiter
+	 * @return String array splited by delimiter
+	 */
+	private String[] split(String numbersText, String delimiter) {
+		return numbersText.split(delimiter);
+	}
 
 }
