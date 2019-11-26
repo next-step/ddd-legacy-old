4ca7c99236dcd10f19792fb92b730077d5deeff0
isooo <midnightmarching@gmail.com>
2019-11-26 13:14:54 +0900

    refactor (calculator): #58 리뷰 반영 : Objects.isNull 사용, if문 제거

--- src/main/java/camp/nextstep/edu/calculator/StringUtils.java
+++ src/main/java/camp/nextstep/edu/calculator/StringUtils.java
@@ -1,10 +1,9 @@
 package camp.nextstep.edu.calculator;
 
+import java.util.Objects;
+
 public class StringUtils {
     public static boolean isEmpty(final String value) {
-        if (value == null || value.isEmpty() || value.trim().length() == 0) {
-            return true;
-        }
-        return false;
+        return Objects.isNull(value) || value.isEmpty() || value.trim().isEmpty();
     }
 }
