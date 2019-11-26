5724a0c3fe560d82986315b0c5a9d1950fe842f7
Ryan.an <giveawesome@gmail.com>
2019-11-26 21:41:40 +0900

    refactor(StringCalculator): applying changed method "parseInt" which is related with refactoring class "Number"

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -1,17 +1,16 @@
 package camp.nextstep.edu.calculator;
 
-import camp.nextstep.edu.calculator.exception.NegativeNumberException;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.springframework.util.StringUtils;
+import camp.nextstep.edu.calculator.util.Number;
 
 public class StringCalculator {
 
     private static final String DELIMITER = ",|:";
     private static final String PATTERN_DELIMITER = "//(.)\n(.*)";
     private static final Pattern PATTERN = Pattern.compile(PATTERN_DELIMITER);
-    private static final int ZERO_VALUE = 0;
 
     /**
      * Add string of integer numbers which is included with comma(,) or colon(:)
@@ -42,34 +41,8 @@ public class StringCalculator {
      */
     private int sum(String[] numbers) {
         return Arrays.stream(numbers)
-            .mapToInt(this::parseInt)
+            .mapToInt(Number::parseInt)
             .sum();
-    }
-
-    /**
-     * Parse string number into integer number
-     *
-     * @param number
-     * @return parsed integer value
-     */
-    private int parseInt(final String number) {
-        int parsedNumber = Integer.parseInt(number);
-
-        if (isNegativeNumber(parsedNumber)) {
-            throw new NegativeNumberException(parsedNumber);
-        }
-
-        return parsedNumber;
-    }
-
-    /**
-     * Check whether positive number or not
-     *
-     * @param number
-     * @return boolean value whether positive number or not
-     */
-    private boolean isNegativeNumber(final int number) {
-        return number < ZERO_VALUE;
     }
 
     /**
