78e399f325b47f42ec01ae0a30cfae413dda2996
Ryan.an <giveawesome@gmail.com>
2019-11-26 20:35:11 +0900

    refactor(StringCalculator): heavily used logic to change pattern

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -8,78 +8,79 @@ import org.springframework.util.StringUtils;
 
 public class StringCalculator {
 
-	private static final String DELIMITER = ",|:";
-	private static final String PATTERN_DELIMITER = "//(.)\n(.*)";
-	private static final int ZERO_VALUE = 0;
+    private static final String DELIMITER = ",|:";
+    private static final String PATTERN_DELIMITER = "//(.)\n(.*)";
+    private static final Pattern PATTERN = Pattern.compile(PATTERN_DELIMITER);
+    private static final int ZERO_VALUE = 0;
 
-	/**
-	 * Add string of integer numbers which is included with comma(,) or colon(:)
-	 *
-	 * @param numbersText
-	 * @return sum of integer numbers
-	 */
-	public int add(final String numbersText) {
-		if (StringUtils.isEmpty(numbersText)) {
-			return 0;
-		}
+    /**
+     * Add string of integer numbers which is included with comma(,) or colon(:)
+     *
+     * @param numbersText
+     * @return sum of integer numbers
+     */
+    public int add(final String numbersText) {
+        if (StringUtils.isEmpty(numbersText)) {
+            return 0;
+        }
 
-		Matcher matcher = Pattern.compile(PATTERN_DELIMITER).matcher(numbersText);
+        Matcher matcher = PATTERN.matcher(numbersText);
 
-		if (matcher.find()) {
-			String patternDelimeter = matcher.group(1);
-			return sum(split(matcher.group(2), patternDelimeter));
-		}
+        if (matcher.find()) {
+            String patternDelimeter = matcher.group(1);
+            return sum(split(matcher.group(2), patternDelimeter));
+        }
 
-		return sum(split(numbersText, DELIMITER));
-	}
+        return sum(split(numbersText, DELIMITER));
+    }
 
-	/**
-	 * Sum of integer numbers
-	 *
-	 * @param numbers
-	 * @return sum of integer numbers
-	 */
-	private int sum(String[] numbers) {
-		return Arrays.stream(numbers)
-			.mapToInt(this::parseInt)
-			.sum();
-	}
+    /**
+     * Sum of integer numbers
+     *
+     * @param numbers
+     * @return sum of integer numbers
+     */
+    private int sum(String[] numbers) {
+        return Arrays.stream(numbers)
+            .mapToInt(this::parseInt)
+            .sum();
+    }
 
-	/**
-	 * Parse string number into integer number
-	 *
-	 * @param number
-	 * @return parsed integer value
-	 */
-	private int parseInt(final String number) {
-		int parsedNumber = Integer.parseInt(number);
+    /**
+     * Parse string number into integer number
+     *
+     * @param number
+     * @return parsed integer value
+     */
+    private int parseInt(final String number) {
+        int parsedNumber = Integer.parseInt(number);
 
-		if (isNegativeNumber(parsedNumber)) {
-			throw new NegativeNumberException(parsedNumber);
-		}
+        if (isNegativeNumber(parsedNumber)) {
+            throw new NegativeNumberException(parsedNumber);
+        }
 
-		return parsedNumber;
-	}
+        return parsedNumber;
+    }
 
-	/**
-	 * Check whether positive number or not
-	 *
-	 * @param number
-	 * @return boolean value whether positive number or not
-	 */
-	private boolean isNegativeNumber(final int number) {
-		return number < ZERO_VALUE;
-	}
+    /**
+     * Check whether positive number or not
+     *
+     * @param number
+     * @return boolean value whether positive number or not
+     */
+    private boolean isNegativeNumber(final int number) {
+        return number < ZERO_VALUE;
+    }
 
-	/**
-	 * Split string of integer numbers which is included with delimiter
-	 *
-	 * @param numbersText
-	 * @param delimiter
-	 * @return String array splited by delimiter
-	 */
-	private String[] split(String numbersText, String delimiter) {
-		return numbersText.split(delimiter);
-	}
+    /**
+     * Split string of integer numbers which is included with delimiter
+     *
+     * @param numbersText
+     * @param delimiter
+     * @return String array splited by delimiter
+     */
+    private String[] split(String numbersText, String delimiter) {
+        return numbersText.split(delimiter);
+    }
 
 }
