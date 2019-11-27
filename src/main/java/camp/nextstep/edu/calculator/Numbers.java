0ef6c280c9562ab2e65b933fbf7b3c44303ac42a
ohtaeg <otk1090@naver.com>
2019-11-27 21:22:31 +0900

    refactor (string calculator) : Numbers 클래스 정적 팩터리 메서드 추가 및 sum() 메서드 추가
    - StringCalculator 클래스에서 계산이 아닌 메시지를 통해 Numbers클래스에서 데이터를 다루도록 수정.

--- src/main/java/camp/nextstep/edu/calculator/Numbers.java
+++ src/main/java/camp/nextstep/edu/calculator/Numbers.java
@@ -1,7 +1,9 @@
 package camp.nextstep.edu.calculator;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public final class Numbers {
     private final List<Number> numbers;
@@ -10,7 +12,32 @@ public final class Numbers {
         this.numbers = numbers;
     }
 
-    public static Numbers intValuesOf(String[] separatedTex) {
-        return new Numbers(new ArrayList<Number>());
+    public static Numbers intValuesOf(String[] separatedText) {
+        List<Number> numbers = new ArrayList<>(separatedText.length);
+        for (String text : separatedText) {
+            numbers.add(Number.intValueOf(text));
+        }
+        return new Numbers(Collections.unmodifiableList(numbers));
+    }
+
+    public Number sum() {
+        int sum = 0;
+        for (Number number : numbers) {
+            sum += number.getValue();
+        }
+        return Number.intValueOf(String.valueOf(sum));
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Numbers numbers1 = (Numbers) o;
+        return Objects.equals(numbers, numbers1.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
     }
 }
