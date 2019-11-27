caad18c34f4ca07104d3ce73a023b0ae7ce94d8e
ohtaeg <otk1090@naver.com>
2019-11-27 21:29:06 +0900

    test (string calculator) : Numbers 클래스 sum() 메서드 테스트 코드 추가

--- src/test/java/camp/nextstep/edu/calculator/NumbersTest.java
+++ src/test/java/camp/nextstep/edu/calculator/NumbersTest.java
@@ -2,7 +2,12 @@ package camp.nextstep.edu.calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.test.context.junit.jupiter.DisabledIf;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,11 +19,25 @@ class NumbersTest {
 
     @DisplayName("String 배열을 파라미터로 던지면 Numbers 객체의 인스턴스를 생성할 수 있다.")
     @Test
-    void intValuesOf() {
+    void create() {
         // when
         numbers = numbers.intValuesOf(new String[]{"1", "2"});
 
         // then
         assertThat(numbers).isNotNull();
+    }
+
+    @DisplayName("정상적인 문자배열을 던지면 Numbers 객체 생성후 합을 구할 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"'1,2'=3", "'1,2,3'=6"}, delimiter = '=')
+    void sum(String input, int expected) {
+        // given
+        String[] splitedInput = input.split(",");
+
+        // when
+        numbers = numbers.intValuesOf(splitedInput);
+
+        // then
+        assertThat(numbers.sum().getValue()).isEqualTo(expected);
     }
 }
