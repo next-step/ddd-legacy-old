0f91897df342352fb62da59cc964ec3521c47059
kojunhee <junheee.ko@gmail.com>
2019-11-30 15:37:30 +0900

    refactor : Pattern 을 미리 만들어둠, string->integer 책임 위임

--- src/main/java/camp/nextstep/edu/calculator/StringParser.java
+++ src/main/java/camp/nextstep/edu/calculator/StringParser.java
@@ -4,12 +4,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class StringParser {
-    private final String REGEX = "//(.)\n(.*)";
-    private final String DELIMITER = ",|:";
+    private static final String REGEX = "//(.)\n(.*)";
+    private static final String DELIMITER = ",|:";
+    private static final Pattern REGEX_PATTERN = Pattern.compile(REGEX);
 
-    String[] parse(String text) {
-        Matcher m = Pattern.compile(REGEX).matcher(text);
+    String[] toStringArrayFrom(String text) {
+        Matcher m = REGEX_PATTERN.matcher(text);
 
         return m.find() ? m.group(2).split(m.group(1)) : text.split(DELIMITER);
+    }
+
+    Integer toIntFrom(String text){
+        return Integer.parseInt(text);
     }
 }
